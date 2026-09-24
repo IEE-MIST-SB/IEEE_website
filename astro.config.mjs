ef626e7bcc0034bd2ffbff7b632f08754f887615
Syed-Fairuz-Mahdi <fairuzmahdi44@gmail.com>
2026-09-24 14:39:51 +0600

    Update site and base URL in astro.config.mjs to show styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://IEE-MIST-SB.github.io',
-	base: '/IEEE_website',
+  site: 'https://ieee-mist-sb.vercel.app',
+  base: '/',
 });
